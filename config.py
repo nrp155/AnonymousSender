9cde8b82e5f88f9e22f3d03e2147ce8150d73001
nrp155 <84077310+nrp155@users.noreply.github.com>
2024-04-04 10:49:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 import os
 
 import os
-API_ID = int(os.getenv("4399087"))
-API_HASH = os.getenv("112023ea7546c4f071e0b195ea5996a5")
-BOT_TOKEN = os.getenv("6946445545:AAFi7Pi8QZhWU8sDRIVn3DPGEZpRPRan7cU")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
